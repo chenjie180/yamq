e1cdae6655c03dcfe8ead2d19bc0e495f0d9f5ef
chenjie@sparknet.com.cn <chenjie@sparknet.com.cn@192.168.120.139>
2020-08-07 08:59:16 +0800

    延时队列

---
+++ springboot-ysmq/src/main/java/com/example/demo/SpringbootYsmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootYsmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootYsmqApplication.class, args);
+	}
+
+}
